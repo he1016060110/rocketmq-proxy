396784c97c0ac0bed80826605f5fe4f74a28f086
hexi <hexi@hexideMac-Pro.lan>
2020-02-25 14:20:55 +0800

    命名方式修改

--- ProxyServer.cpp
+++ ProxyServer.cpp
@@ -11,7 +11,7 @@ using namespace rocketmq;
 
 SendCallback* g_callback = NULL;
 
-class MySendCallback : public SendCallback {
+class ProducerCallback : public SendCallback {
     shared_ptr<WsServer::Connection> conn;
     virtual void onSuccess(SendResult& sendResult) {
         this->conn->send(sendResult.getMsgId(), [](const SimpleWeb::error_code &ec) {
@@ -40,60 +40,55 @@ int main() {
     server.config.port = 8080;
 
     DefaultMQProducer * producer = new DefaultMQProducer("AsyncProducer");
+
     producer->setNamesrvAddr("namesrv:9876");
     producer->setInstanceName("AsyncProducer");
     producer->setSendMsgTimeout(500);
     producer->setTcpTransportTryLockTimeout(1000);
     producer->setTcpTransportConnectTimeout(400);
     producer->start();
+    
+    auto &producerEndpoint = server.endpoint["^/producerEndpoint/?$"];
 
-    // Example 1: echo WebSocket endpoint
-    // Added debug messages for example use of the callbacks
-    // Test with the following JavaScript:
-    //   var ws=new WebSocket("ws://localhost:8080/echo");
-    //   ws.onmessage=function(evt){console.log(evt.data);};
-    //   ws.send("test");
-    auto &echo = server.endpoint["^/echo/?$"];
-
-    echo.on_message = [&producer](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
+    producerEndpoint.on_message = [&producer](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
         auto out_message = in_message->string();
         rocketmq::MQMessage msg("TestTopicAsync",  // topic
                                 "*",          // tag
                                 "test message!");  // body
-        MySendCallback * calllback = new MySendCallback();
+        ProducerCallback * calllback = new ProducerCallback();
         calllback->setConn(connection);
         producer->send(msg, calllback);
     };
 
-    echo.on_open = [](shared_ptr<WsServer::Connection> connection) {
+    producerEndpoint.on_open = [](shared_ptr<WsServer::Connection> connection) {
         cout << "Server: Opened connection " << connection.get() << endl;
     };
 
     // See RFC 6455 7.4.1. for status codes
-    echo.on_close = [](shared_ptr<WsServer::Connection> connection, int status, const string & /*reason*/) {
+    producerEndpoint.on_close = [](shared_ptr<WsServer::Connection> connection, int status, const string & /*reason*/) {
         cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
     };
 
     // Can modify handshake response headers here if needed
-    echo.on_handshake = [](shared_ptr<WsServer::Connection> /*connection*/, SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
+    producerEndpoint.on_handshake = [](shared_ptr<WsServer::Connection> /*connection*/, SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
         return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
     };
 
     // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
-    echo.on_error = [](shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
+    producerEndpoint.on_error = [](shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
         cout << "Server: Error in connection " << connection.get() << ". "
              << "Error: " << ec << ", error message: " << ec.message() << endl;
     };
 
-    // Example 2: Echo thrice
+    // Example 2: producerEndpoint thrice
     // Demonstrating queuing of messages by sending a received message three times back to the client.
     // Concurrent send operations are automatically queued by the library.
     // Test with the following JavaScript:
-    //   var ws=new WebSocket("ws://localhost:8080/echo_thrice");
+    //   var ws=new WebSocket("ws://localhost:8080/producerEndpoint_thrice");
     //   ws.onmessage=function(evt){console.log(evt.data);};
     //   ws.send("test");
-    auto &echo_thrice = server.endpoint["^/echo_thrice/?$"];
-    echo_thrice.on_message = [](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
+    auto &producerEndpoint_thrice = server.endpoint["^/producerEndpoint_thrice/?$"];
+    producerEndpoint_thrice.on_message = [](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
         auto out_message = make_shared<string>(in_message->string());
 
         connection->send(*out_message, [connection, out_message](const SimpleWeb::error_code &ec) {
@@ -103,17 +98,17 @@ int main() {
         connection->send(*out_message); // Most likely queued. Sent after the first send operation is finished.
     };
 
-    // Example 3: Echo to all WebSocket endpoints
+    // Example 3: producerEndpoint to all WebSocket endpoints
     // Sending received messages to all connected clients
     // Test with the following JavaScript on more than one browser windows:
-    //   var ws=new WebSocket("ws://localhost:8080/echo_all");
+    //   var ws=new WebSocket("ws://localhost:8080/producerEndpoint_all");
     //   ws.onmessage=function(evt){console.log(evt.data);};
     //   ws.send("test");
-    auto &echo_all = server.endpoint["^/echo_all/?$"];
-    echo_all.on_message = [&server](shared_ptr<WsServer::Connection> /*connection*/, shared_ptr<WsServer::InMessage> in_message) {
+    auto &producerEndpoint_all = server.endpoint["^/producerEndpoint_all/?$"];
+    producerEndpoint_all.on_message = [&server](shared_ptr<WsServer::Connection> /*connection*/, shared_ptr<WsServer::InMessage> in_message) {
         auto out_message = in_message->string();
 
-        // echo_all.get_connections() can also be used to solely receive connections on this endpoint
+        // producerEndpoint_all.get_connections() can also be used to solely receive connections on this endpoint
         for(auto &a_connection : server.get_connections())
             a_connection->send(out_message);
     };
