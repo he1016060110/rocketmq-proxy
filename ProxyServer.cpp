88dbea27d38e76de2f317d86a6bcfae9b9443ae3
hexi <hexi@hexideMac-Pro.lan>
2020-02-25 17:09:56 +0800

    consumer proxy

--- ProxyServer.cpp
+++ ProxyServer.cpp
@@ -61,6 +61,7 @@ int main() {
     auto &producerEndpoint = server.endpoint["^/producerEndpoint/?$"];
     auto &consumerEndpoint = server.endpoint["^/consumerEndpoint/?$"];
 
+    //producer proxy
     producerEndpoint.on_message = [&wp](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
         string json = in_message->string();
         std::istringstream jsonStream;
@@ -90,14 +91,15 @@ int main() {
         return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
     };
 
-    // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
     producerEndpoint.on_error = [](shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
         cout << "Server: Error in connection " << connection.get() << ". "
              << "Error: " << ec << ", error message: " << ec.message() << endl;
     };
 
+    //consumer proxy
     consumerEndpoint.on_message = [&wp](shared_ptr<WsServer::Connection> connection, shared_ptr<WsServer::InMessage> in_message) {
         cout << in_message->string() << "\n";
+        connection->send(in_message->string());
     };
 
     consumerEndpoint.on_open = [](shared_ptr<WsServer::Connection> connection) {
@@ -108,12 +110,10 @@ int main() {
         cout << "Server: Closed connection " << connection.get() << " with status code " << status << endl;
     };
 
-    // Can modify handshake response headers here if needed
     consumerEndpoint.on_handshake = [](shared_ptr<WsServer::Connection> /*connection*/, SimpleWeb::CaseInsensitiveMultimap & /*response_header*/) {
         return SimpleWeb::StatusCode::information_switching_protocols; // Upgrade to websocket
     };
 
-    // See http://www.boost.org/doc/libs/1_55_0/doc/html/boost_asio/reference.html, Error Codes for error code meanings
     consumerEndpoint.on_error = [](shared_ptr<WsServer::Connection> connection, const SimpleWeb::error_code &ec) {
         cout << "Server: Error in connection " << connection.get() << ". "
              << "Error: " << ec << ", error message: " << ec.message() << endl;
