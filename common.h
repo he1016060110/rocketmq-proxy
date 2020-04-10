3c97a0885c25d7b682105ac6eb79b05dc6066926
hexi <hexi@hexideMac-Pro.lan>
2020-04-10 17:05:08 +0800

    拆分文件

---
+++ common.h
@@ -0,0 +1,13 @@
+//
+// Created by hexi on 2020/4/10.
+//
+
+#ifndef ROCKETMQ_PROXY_COMMON_H
+#define ROCKETMQ_PROXY_COMMON_H
+
+
+#define PRINT_ERROR(e) do { \
+  cout << "file: " << __FILE__ << " line: " << __LINE__ << " msg: " << e.what() << endl; \
+} while(0)
+
+#endif //ROCKETMQ_PROXY_COMMON_H
